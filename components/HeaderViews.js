6ef908f862f26b7dfb00b232392650aa932506ac
diegoezc <diegoezc@gmail.com>
2024-06-05 18:31:02 -0400

    cambios del header

--- components/HeaderViews.js
+++ components/HeaderViews.js
@@ -8,7 +8,7 @@ const {width, height} = Dimensions.get('window')
 
 export default function HeaderViews() {
   return (
-    <View>
+    <View style={tw`w-full flex justify-center items-center h-55 bg-[#25B201] `}>
     <Image 
         style={{width: 120, height: 120, marginTop: 40}}
         source={require('../assets/logo-mu.png')
